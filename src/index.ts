98f6f1f87fd68b64eab038bea76ab7ed740c7dca
Miquel Llufriu <miguelllufriu@gmail.com>
2018-08-30 21:09:44 +0200

    Defined our server in a class for better and clean code

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,3 @@
-import * as express from 'express';
-import * as socketio from 'socket.io';
-import { WelcomeController } from './basic_controllers'
+import { SocketServer } from './socket_server/socketServer';
 
-//Setup our express app
-const app: express.Application = express();
-const port: number = 3000;
-
-//Setup and bind our socket server
-let httpServer = require('http').Server(app);
-let io = require('socket.io')(httpServer);
-
-//Basic express controller for testing only
-app.use('/welcome', WelcomeController);
-
-//On connection with socket server
-io.on('connection', (socket: SocketIO.Socket) => {
-    console.log('New user connected!');
-});
-
-//Deploy our express app
-app.listen(port, () => {
-    console.log("[INFO] Server up and running at http://localhost:3000");
-});
+let app = new SocketServer().getServer();
---
+++ src/socket_server/socketServer.ts
@@ -0,0 +1,52 @@
+import { createServer, Server } from 'http';
+import * as express from 'express';
+import * as socketio from 'socket.io';
+
+export class SocketServer {
+
+    private static readonly port: number = 1234;
+    private app: express.Application;
+    private server: Server;
+    private ioServer: SocketIO.Server;
+    private port: number;
+    
+    constructor() {
+        this.createApp();
+        this.config();
+        this.createServer();
+        this.sockets();
+        this.listen();
+    }
+
+    private createApp(): void {
+        this.app = express();
+    }
+
+    private createServer(): void {
+        this.server = createServer(this.app);
+    }
+
+    private config(): void {
+        this.port = SocketServer.port;
+    }
+
+    private sockets(): void {
+        this.ioServer = socketio(this.server);
+    }
+
+    private listen(): void{
+        this.app.listen(this.port, () =>{
+            console.log('[START] Running server on port', this.port);
+        });
+        this.ioServer.on('connection', () => {
+            console.info('New user connected!')
+        });
+        this.ioServer.on('disconnect', () => {
+            console.warn('User disconnected!')
+        });
+    }
+
+    public getServer(): express.Application{
+        return this.app;
+    } 
+}
